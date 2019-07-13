13e6a8a9a896c33e385a26d8bff057fee8d05ee0
medehn <medehn85@gmail.com>
2019-07-13 10:48:02 +0200

    movement and rotation added

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float moveSpeed;
+    public float rotSpeed;
+    
+    
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //movement of lower part with simple horizontal and vertical input, not very "tankish"
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0.0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+        
+    }
+    public float speed;
+ 
+
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate () 
+    {
+        // Generate a plane that intersects the transform's position with an upwards normal.
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+ 
+        // Generate a ray from the cursor position
+        Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+ 
+        // Determine the point where the cursor ray intersects the plane.
+        float hitdist = 0.0f;
+        // If the ray is parallel to the plane, Raycast will return false.
+        if (playerPlane.Raycast (ray, out hitdist)) 
+        {
+            // Get the point along the ray that hits the calculated distance.
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+ 
+            // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+ 
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
+    }
+}
